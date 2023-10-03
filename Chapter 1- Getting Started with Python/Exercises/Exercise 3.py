5789115cbab98ec370c0444883b26ff342d8aaff
hajara munjiya <hajaramunjiya23@gmail.com>
2023-10-03 12:14:52 +0400

    exercise 3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 3.py
@@ -0,0 +1,23 @@
+# python3 code to demonstrate
+# attributes of now ()
+
+# importing datetime module for now()
+import datetime
+
+# using now() to get current time
+current_time= datetime.datetime.now()
+
+#printing attributes of now()
+print("the attributes of now() are :")
+
+print("year :" , current_time.year)
+
+print("month :" , current_time.month)
+
+print("day :" , current_time.day)
+
+print("hour :" , current_time.hour)
+
+print("minute :" , current_time.minute)
+
+print("second :" , current_time.second)
