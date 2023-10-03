446f42f4500bbb1c60c72863a1e07ebf4a55a2da
hajara munjiya <hajaramunjiya23@gmail.com>
2023-10-03 12:30:22 +0400

    excercise 4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 4.py
@@ -0,0 +1,11 @@
+def join_string(str1, str2, str3):
+    #concatenate and return the strings
+    return str1 + " " + str2 + " " + str3
+
+#define the strings as part of a joke
+string1 = "I"
+string2 = "am"
+string3 = "Hajara"
+
+#call the funtion with strings as arguments 
+print(join_string(string1, string2,string3 ))
