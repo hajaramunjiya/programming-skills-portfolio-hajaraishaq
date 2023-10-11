bf0a7cd1288e1c18f80c9892bf434ace768f86f2
hajara munjiya <hajaramunjiya23@gmail.com>
2023-10-11 11:52:45 +0400

    exercise 2.2.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 2.2.py
@@ -0,0 +1,4 @@
+print('Maya Angelou once said, "Ive learned that people will forget what you said,')
+print(' people will forget what you did, but people will never forget how you made them feel."')
+      
+       
